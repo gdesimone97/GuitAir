397d0ffdbf4fc4979c6da2bfa53be073b12b0499
Mario <mariodesio1@gmail.com>
2019-07-15 16:39:09 +0200

    Schermata home e gioco

--- LetsPlayStoryboards/ViewController.swift
+++ LetsPlayStoryboards/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var deviceStatus: UIView!
+    
+    
+    // Chords name displayed in home
+    
+    @IBOutlet weak var firstChordLabel: UILabel!
+    
+    @IBOutlet weak var secondChordLabel: UILabel!
+    
+    @IBOutlet weak var thirdChordLabel: UILabel!
+    
+    @IBOutlet weak var fourthChordLabel: UILabel!
+    
+    
+    // Labels shown in game mode, each label is associated with a button
+    
+    @IBOutlet weak var redButtonChord: UILabel!
+    
+    @IBOutlet weak var blueButtonChord: UILabel!
+    
+    @IBOutlet weak var greenButtonChord: UILabel!
+    
+    @IBOutlet weak var pinkButtonChord: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // If device is paried, the background color will be green, if not it will be yellow
+        deviceStatus?.backgroundColor = UIColor(red: 0.3, green: 1, blue: 0.17, alpha: 1)
+        deviceStatus?.layer.cornerRadius = 8.34
+        
+        
+        // Label rotation in game mode
+        
+        
+        redButtonChord?.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        blueButtonChord?.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        greenButtonChord?.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        pinkButtonChord?.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        
+        // Updating of chords label 
+        fourthChordLabel?.text = "Gm"
     }
-
 
 }
 
