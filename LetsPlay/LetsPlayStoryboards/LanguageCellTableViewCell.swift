c364f370a30543922bb7c4ccee9ce808de58d589
christian <chsmrn@gmail.com>
2019-07-16 10:15:11 +0200

    chordPicker che va

---
+++ LetsPlay/LetsPlayStoryboards/LanguageCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  LanguageCellTableViewCell.swift
+//  LetsPlayStoryboards
+//
+//  Created by Christian Marino on 16/07/2019.
+//  Copyright © 2019 Christian Marino. All rights reserved.
+//
+
+import UIKit
+
+class LanguageCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var langName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
